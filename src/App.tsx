e500aeb159173414ac3ded5e177c6e1aecec3825
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-06-17 13:57:45 +0200

    Nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./Componets/NavBar";
+//import ProductForm from "./Componets/ProductForm";
+//import ProductList from "./Componets/ProductList";
 
 export default function App() {
     return (
         <div>
             <NavBar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="about" element={<About />} />
+                <Route path="/" element={"hello"} />
+                <Route path="products" element={<ProductList />} />
+                <Route path="addProduct" element={<ProductForm />} />
             </Routes>
         </div>
     );
--- src/Componets/NavBar.tsx
+++ src/Componets/NavBar.tsx
@@ -1,15 +1,23 @@
 import { NavLink } from "react-router-dom";
 
-export default function NavBar() { 
-
+export default function NavBar() {
     return (
-        <nav>
-            <ul>
+        <nav className="bg-slate-600 p-6">
+            <ul className="flex justify-between">
                 <li>
-                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/" end className="text-white hover:text-gray-300">
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="about">About</NavLink>
+                    <NavLink to="products" className="text-white hover:text-gray-300">
+                        Products
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="addProduct" className="text-white hover:text-gray-300">
+                        Add Product
+                    </NavLink>
                 </li>
             </ul>
         </nav>
